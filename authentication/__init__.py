199f9ff4b4871a6b880e9018ca83707562b26ca4
Deep <deep.inexture@gmail.com>
2022-05-27 15:08:40 +0530

    Project Deploy

---
+++ authentication/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dd029fcb563da7a02897bafff2efe736'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from authentication import routes
---
+++ authentication/routes.py
@@ -0,0 +1,53 @@
+from flask import render_template, url_for, flash, redirect
+from authentication import app, db, bcrypt
+from authentication.forms import RegistrationForm, LoginForm
+from authentication.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title='Home')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('about'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your Account has been Created! You are now able to Log In', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title=' Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('about'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return render_template('about.html', title='About', form=form)
+        else:
+            flash('Login Unsuccessful. Please Check Your Username and Password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/about")
+@login_required
+def about():
+    return render_template('about.html', title='About')
